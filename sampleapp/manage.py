dca34ac433f3392b7c580845e988a8695afd8780
omtripathi88 <om.tripathi@clariontechnologies.co.in>
2014-10-30 17:00:58 +0530

    smaple for test branch

--- sampleapp/manage.py
+++ sampleapp/manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 from collections import defaultdict
+from datetime import datetime
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleapp.settings")
